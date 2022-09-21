81025deb0b647e6c3a3ebe708ccd12f42f0e148c
Vince Ermitano <93059468+vince-ermitano@users.noreply.github.com>
2022-09-21 15:33:27 -0700

    Time: 1 ms (81.30%), Space: 42.2 MB (84.39%) - LeetHub

--- 21-merge-two-sorted-lists/21-merge-two-sorted-lists.java
+++ 21-merge-two-sorted-lists/21-merge-two-sorted-lists.java
@@ -10,9 +10,47 @@
  */
 class Solution {
     public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
-        ListNode ans;
-        ListNode newListPointer;
+        //iterative approach
+//         ListNode ans;
+//         ListNode newListPointer;
         
+//         if (list1 == null) {
+//             return list2;
+//         }
+//         if (list2 == null) {
+//             return list1;
+//         }
+//         if (list1.val <= list2.val) {
+//             ans = new ListNode(list1.val);
+//             list1 = list1.next;
+//         } else {
+//             ans = new ListNode(list2.val);
+//             list2 = list2.next;
+//         }
+        
+//         newListPointer = ans;
+        
+//         while (list1 != null || list2 != null) {
+//             if (list1 == null) {
+//                 newListPointer.next = list2;
+//                 break;
+//             }
+//             if (list2 == null) {
+//                 newListPointer.next = list1;
+//                 break;
+//             }
+//             if (list1.val <= list2.val) {
+//                 newListPointer.next = new ListNode(list1.val);
+//                 list1 = list1.next;
+//             } else {
+//                 newListPointer.next = new ListNode(list2.val);
+//                 list2 = list2.next;
+//             }
+//             newListPointer = newListPointer.next;
+//         }
+//         return ans;
+        
+        //recursive approach
         if (list1 == null) {
             return list2;
         }
@@ -20,33 +58,11 @@ class Solution {
             return list1;
         }
         if (list1.val <= list2.val) {
-            ans = new ListNode(list1.val);
-            list1 = list1.next;
+            list1.next = mergeTwoLists(list1.next, list2);
+            return list1;
         } else {
-            ans = new ListNode(list2.val);
-            list2 = list2.next;
+            list2.next = mergeTwoLists(list1, list2.next);
+            return list2;
         }
-        
-        newListPointer = ans;
-        
-        while (list1 != null || list2 != null) {
-            if (list1 == null) {
-                newListPointer.next = list2;
-                break;
-            }
-            if (list2 == null) {
-                newListPointer.next = list1;
-                break;
-            }
-            if (list1.val <= list2.val) {
-                newListPointer.next = new ListNode(list1.val);
-                list1 = list1.next;
-            } else {
-                newListPointer.next = new ListNode(list2.val);
-                list2 = list2.next;
-            }
-            newListPointer = newListPointer.next;
-        }
-        return ans;
     }
 }
