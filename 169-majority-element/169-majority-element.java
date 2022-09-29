771ba0aed8346c98d4171cfe5602dffba3a48338
Vince Ermitano <93059468+vince-ermitano@users.noreply.github.com>
2022-09-29 10:56:11 -0700

    Time: 7 ms (49.10%), Space: 57.2 MB (6.01%) - LeetHub

--- 169-majority-element/169-majority-element.java
+++ 169-majority-element/169-majority-element.java
@@ -1,22 +1,7 @@
 class Solution {
     public int majorityElement(int[] nums) {
-        HashMap<Integer, Integer> count = new HashMap<Integer, Integer>();
-        int ans = -1;
+        Arrays.sort(nums);
         
-        for (int n : nums) {
-            if (count.containsKey(n)) {
-                count.replace(n, count.get(n) + 1);
-            } else {
-                count.put(n, 1);
-            }
-        }
-        
-        for (int key : count.keySet()) {
-            if (count.get(key) > nums.length / 2) {
-                ans = key;
-            }
-        }
-        
-        return ans;
+        return nums[nums.length / 2];
     }
 }
