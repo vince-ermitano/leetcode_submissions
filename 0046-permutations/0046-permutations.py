f833e06da2f6aa2d537cfa6b91f0c31b89fc8157
Vince Ermitano <93059468+vince-ermitano@users.noreply.github.com>
2022-10-26 16:02:37 -0700

    Time: 64 ms (13.03%), Space: 13.4 MB (85.61%) - LeetHub

---
+++ 0046-permutations/0046-permutations.py
@@ -0,0 +1,27 @@
+class Solution(object):
+    def permute(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[List[int]]
+        """
+        permutations = []
+        n = len(nums)
+        
+        def backtrack(first):
+            if first == n:
+                permutations.append(nums[:])
+            for i in range(first, n):
+                temp = nums[first]
+                nums[first] = nums[i]
+                nums[i] = temp
+                
+                backtrack(first+1)
+                
+                temp = nums[first]
+                nums[first] = nums[i]
+                nums[i] = temp
+        
+        backtrack(0)
+        return permutations
+                
+        
