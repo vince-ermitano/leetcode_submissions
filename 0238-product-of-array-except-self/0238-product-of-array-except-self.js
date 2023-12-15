cf3609b82cc77aa3b9b77e5d3b72f22538f0d2ff
Vince Ermitano <93059468+vince-ermitano@users.noreply.github.com>
2023-12-15 15:25:19 -0800

    Time: 93 ms (41.75%), Space: 53.1 MB (85.27%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.js
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.js
@@ -3,34 +3,20 @@
  * @return {number[]}
  */
 var productExceptSelf = function(nums) {
-    const leftProducts = [];
-    const rightProducts = [];
-    const ans = [];
+    const ans = Array(nums.length).fill(1);
     
-    for (const num of nums) {
-        if (leftProducts.length === 0) {
-            leftProducts.push(num);
-        } else {
-            leftProducts.push(leftProducts[leftProducts.length - 1] * num)
-        }
-    }
-    
-    for (let i = nums.length-1; i >= 0; i--) {
-        if (rightProducts.length === 0) {
-            rightProducts.push(nums[i]);
-        } else {
-            rightProducts.unshift(rightProducts[0] * nums[i]);
-        }
-    }
+    let prefix = 1;
     
     for (let i = 0; i < nums.length; i++) {
-        if (i === 0) {
-            ans.push(rightProducts[1]);
-        } else if (i === nums.length-1) {
-            ans.push(leftProducts[leftProducts.length-2])
-        } else {
-            ans.push(leftProducts[i-1] * rightProducts[i+1]);
-        }
+        ans[i] = prefix;
+        prefix *= nums[i];
+    }
+    
+    let postfix = 1;
+    
+    for (let i = nums.length-1; i >= 0; i--) {
+        ans[i] *= postfix;
+        postfix *= nums[i];
     }
     
     return ans;
