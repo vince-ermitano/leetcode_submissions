034d06cc5b685381197f7b65e4cfcbf88e70092c
Vince Ermitano <93059468+vince-ermitano@users.noreply.github.com>
2023-12-26 14:24:32 -0800

    Time: 49 ms (72.32%), Space: 42.4 MB (15.93%) - LeetHub

--- 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.js
+++ 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.js
@@ -9,15 +9,19 @@ var findMin = function(nums) {
     let right = nums.length-1;
     
     while (left <= right) {
+        
+        if (nums[left] <= nums[right]) {
+            res = Math.min(nums[left], res);
+            break;
+        }
         const mid = Math.floor((left + right) / 2);
         
         res = Math.min(res, nums[mid]);
         
         // left sorted portion
         if (nums[mid] >= nums[left]) {
-            res = Math.min(res, nums[left]);
             left = mid + 1;
-        } else {
+        } else { //right sorted portion
             right = mid-1;
         }
     }
