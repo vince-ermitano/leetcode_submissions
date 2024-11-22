e32f1aa2e58114956c65d156abc2ad9b73c12230
Vince Ermitano <93059468+vince-ermitano@users.noreply.github.com>
2024-11-21 16:56:54 -0800

    Time: 0 ms (100.00%), Space: 53 MB (71.17%) - LeetHub

--- 0231-power-of-two/0231-power-of-two.js
+++ 0231-power-of-two/0231-power-of-two.js
@@ -4,5 +4,10 @@
  */
 var isPowerOfTwo = function(n) {
     
+    /* if n is in fact a power of two, subtracting 1 from its binary form will cause all the 0's to the right of the 1 bit from 1 will turn to 1's.
+        Thus the resulting binary number will have 0's where the original binary number has 1's and will have 1's where the original has 0's. We can then
+        just check if the resulting bitwise & operation between the two numbers results in 0 to confirm that it's a power of 2.
+        */
+    
     return n > 0 && (n & (n-1)) === 0;
 };
