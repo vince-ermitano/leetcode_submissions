cdece284c74e4792882cf16f8c85b8c492ce564d
Vince Ermitano <93059468+vince-ermitano@users.noreply.github.com>
2025-02-22 09:22:05 -0800

    Time: 4 ms, Memory: 12.5 MB - LeetHub

---
+++ 11770-1815-746-min-cost-climbing-stairs/11770-1815-746-min-cost-climbing-stairs.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def minCostClimbingStairs(self, cost):
+        """
+        :type cost: List[int]
+        :rtype: int
+        """
+        dp = [0] * (len(cost)+1)
+
+        for i in range(2, len(cost)+1):
+            dp[i] = min(dp[i-1] + cost[i-1], dp[i-2] + cost[i-2])
+        
+        return dp[len(cost)]
+            
+        
