436cf4b991df79e88ac22870150686a2f8eb4385
Vince Ermitano <93059468+vince-ermitano@users.noreply.github.com>
2025-03-10 12:17:30 -0700

    Time: 4 ms (58.6%), Space: 19.6 MB (54.39%) - LeetHub

--- 0337-house-robber-iii/0337-house-robber-iii.py
+++ 0337-house-robber-iii/0337-house-robber-iii.py
@@ -19,6 +19,8 @@ class Solution:
             left_max = helper(root.left)
             right_max = helper(root.right)
 
+            # if we don't rob the curr house, we need to take the max of the values we could rob
+            # from the prev houses (children) regardless of if we rob/don't rob them
             return [max(left_max) + max(right_max), root.val + left_max[0] + right_max[0]]
 
         return max(helper(root)[0], helper(root)[1])
