cffdf5ee7b034bdefc8ce93ab2a9703d0252d9d8
Vince Ermitano <93059468+vince-ermitano@users.noreply.github.com>
2025-03-13 07:53:16 -0700

    Time: 4 ms (14.53%), Space: 17.7 MB (94.63%) - LeetHub

--- 0443-string-compression/0443-string-compression.py
+++ 0443-string-compression/0443-string-compression.py
@@ -1,27 +1,26 @@
 class Solution:
     def compress(self, chars: List[str]) -> int:
-
-        write = 0  # Position to write in the array
-
-        read = 0  # Position to read from the array
+        write = read = 0
 
         while read < len(chars):
-            char = chars[read]
             count = 0
+            curr_char = chars[read]
 
-            # Count occurrences of the current character
-            while read < len(chars) and chars[read] == char:
+            # increment count while still on same character
+            while read < len(chars) and curr_char == chars[read]:
                 count += 1
                 read += 1
 
-            # Write the character
-            chars[write] = char
+            # write the character to our list
+            chars[write] = curr_char
             write += 1
 
-            # If the count is greater than 1, write its digits
+            # append group length to char if greater than 1
             if count > 1:
-                for digit in str(count):  # Convert number to string and write each digit
+                for digit in str(count):
                     chars[write] = digit
                     write += 1
+        
+        return write
 
-        return write  # The new length of the modified array
+            
