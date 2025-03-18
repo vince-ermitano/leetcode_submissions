d5da4f97f9e71e18bb11d5f0c7196faccfe4cb29
Vince Ermitano <93059468+vince-ermitano@users.noreply.github.com>
2025-03-18 11:20:39 -0700

    Time: 3 ms (93.66%), Space: 18.7 MB (86.86%) - LeetHub

--- 0735-asteroid-collision/0735-asteroid-collision.py
+++ 0735-asteroid-collision/0735-asteroid-collision.py
@@ -5,20 +5,32 @@ class Solution:
         # push asteroids going to the right onto the stack
         # when we encounter an asteroid going to the left
         # compare to the top of the stack and do the collision comparison
-        # if stack is empty or top of stack is a left-directed asteroid
-        # when encountering a left-directed asteroid, add to stack
+        # until asteroid stops colliding
+        # if stack is empty or top of stack is a left-directed asteroid,
+        # add to stack
 
         stack = []
 
+
         for a in asteroids:
+            # add right-directed asteroids
             if a > 0:
                 stack.append(a)
+
+            # encounter left-directed asteroid
             else:
+                # destroy prev asteroids as long as curr asteroid is larger
                 while stack and stack[-1] > 0 and abs(a) > stack[-1]:
                     stack.pop()
                 
+                # if not more asteroids just add curr asteroid
                 if not stack:
                     stack.append(a)
+
+                # handle case where prev asteroid is either
+                # same size as curr asteroid,
+                # a left-directed asteroid,
+                # or larger than the curr asteroid
                 else:
                     prev_a = stack[-1]
 
