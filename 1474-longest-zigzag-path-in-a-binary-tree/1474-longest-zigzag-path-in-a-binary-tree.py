c2bc4e0e27227ae67f45a9ccb98dbc1309db5fc2
Vince Ermitano <93059468+vince-ermitano@users.noreply.github.com>
2025-03-24 13:05:49 -0700

    Time: 88 ms (24.89%), Space: 33.6 MB (22.66%) - LeetHub

--- 1474-longest-zigzag-path-in-a-binary-tree/1474-longest-zigzag-path-in-a-binary-tree.py
+++ 1474-longest-zigzag-path-in-a-binary-tree/1474-longest-zigzag-path-in-a-binary-tree.py
@@ -5,26 +5,22 @@
 #         self.left = left
 #         self.right = right
 class Solution:
+    # keep two lengths as you traverse through the tree (dfs)
+    # cannot go the same direction twice in a row
+    # increase the length if going in the opposite direction
+    # reset the length if going in the same direction
     def longestZigZag(self, root: Optional[TreeNode]) -> int:
-        # dfs to traverse consider each starting node
-        # run another dfs to test length of zigzag path from that node
-        # update res with max length
 
-        longest = 0
+        def dfs(root, left, right):
+            if not root:
+                return max(left, right)
+            
+            # traverse left
+            l = dfs(root.left, right + 1, 0)
+            # traverse right
+            r = dfs(root.right, 0, left + 1)
 
-        def dfs(root, left_length, right_length):
-            nonlocal longest
-
-            longest = max(longest, left_length, right_length)
-
-            if root.left:
-                dfs(root.left, 1 + right_length, 0)
-            if root.right:
-                dfs(root.right, 0, 1 + left_length)
-
-
+            return max(l, r)
         
+        return dfs(root, 0, 0) - 1
 
-        dfs(root, 0, 0)
-
-        return longest
