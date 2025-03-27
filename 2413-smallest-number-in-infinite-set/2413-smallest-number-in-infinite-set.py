2f44df7a2cb6cfb4932b8066ceb996b20f07d971
Vince Ermitano <93059468+vince-ermitano@users.noreply.github.com>
2025-03-27 09:28:31 -0700

    Time: 21 ms (56.3%), Space: 18.5 MB (54.45%) - LeetHub

--- 2413-smallest-number-in-infinite-set/2413-smallest-number-in-infinite-set.py
+++ 2413-smallest-number-in-infinite-set/2413-smallest-number-in-infinite-set.py
@@ -1,19 +1,20 @@
 class SmallestInfiniteSet:
 
     # have a minHeap initialized with just a '1' in it
-    # when we popSmallest, pop from the minHeap and also add the next number
-    # that should be in the heap (keep track with a 'last added' variable)
-    # Just push to heap in addBack
-    # keep track of whats in the set by keeping track of what has gotten removed and added back
+    # when we popSmallest, pop from the minHeap and also add the next number in the sequence
+    # of the infinite set (keep track with a 'last added' variable)
+    # keep track of whats in our q by keeping track of what has gotten removed and added back
+    # addBack will add the number back in our queue if the number has been removed
 
     def __init__(self):
         self.heap = [1]
-        self.lastAdded = 1
-        self.removed = set()
+        self.lastAdded = 1 # keep track of the next number in the sequence
+        self.removed = set() # keep track of what has been removed
         heapq.heapify(self.heap)
 
-        
-
+    # add the next number in the sequence to the heap
+    # pop and add the min value from the heap to our 'removed' list
+    # return the value
     def popSmallest(self) -> int:
         self.lastAdded += 1
         heapq.heappush(self.heap, self.lastAdded)
