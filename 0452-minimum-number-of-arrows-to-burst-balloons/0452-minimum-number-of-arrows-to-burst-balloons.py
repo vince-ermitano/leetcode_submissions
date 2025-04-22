2ab3a5a0f013d172d648c191cb6e0938492afb8b
Vince Ermitano <93059468+vince-ermitano@users.noreply.github.com>
2025-04-22 11:44:56 -0700

    Time: 75 ms (71.56%), Space: 51.5 MB (8.32%) - LeetHub

--- 0452-minimum-number-of-arrows-to-burst-balloons/0452-minimum-number-of-arrows-to-burst-balloons.py
+++ 0452-minimum-number-of-arrows-to-burst-balloons/0452-minimum-number-of-arrows-to-burst-balloons.py
@@ -1,22 +1,20 @@
 # Sort points by x_end
-# Traverse through points maintaining the highest x_start and lowest x_end seen
-# If a point is not between those constraints, we know that we need to throw a
-# new arrow to pop the current balloon.
+# Traverse through points maintaining the x_end of the balloon that we start throwing an arrow with.
+# If a point's x_start is not less than this constraint, we know that we need to throw a
+# new arrow to pop the current balloon and update the right constraint
 class Solution:
     def findMinArrowShots(self, points: List[List[int]]) -> int:
         # sort by x_end
         points.sort(key=lambda x: x[1])
 
         arrows = 1
-        left, right = points[0][0], points[0][1]
+        right = points[0][1]
 
         for x_start, x_end in points:
+            # throw a new arrow when the curr balloon cannot be popped by the curr arrow
             if x_start > right:
                 arrows += 1
-                left, right = x_start, x_end
-            else:
-                left = max(left, x_start)
-                right = min(right, x_end)
+                right = x_end
         
         return arrows
         
